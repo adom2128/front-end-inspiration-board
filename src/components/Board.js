8249bb94da57c6a2cb87a434ec3aa25aeb332a58
Alejandra <adom2128@gmail.com>
2023-06-29 21:58:02 -0600

    sets up skeleton for component files

---
+++ src/components/Board.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Board = ({ id, title, owner }) => {
+
+};
+
+export default Board;
---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Card = ({ id, message, likes_count }) => {
+
+};
+
+export default Card;
