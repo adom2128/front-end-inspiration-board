44ea92a91bcbdd24c257414eb7a9a869ddf4beb5
Danica <danicas.edu@gmail.com>
2023-07-12 10:06:33 -0700

    creates css files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
+import Board from './components/Board'
 import BoardList from './components/BoardList';
 import CardList from './components/CardList';
+
 
 
 function App() {
@@ -25,6 +27,11 @@ function App() {
           />
         </div>
       </main>
+      <footer className="App-footer">
+        <div>
+        &copy; 2023 Alejandra Dominguez, Angie Tran, Cindy Vides, Danica Sarmiento
+        </div>
+      </footer>
     </div>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CardList from './CardList';
 
 const Board = ({ boards }) => {
     return (
