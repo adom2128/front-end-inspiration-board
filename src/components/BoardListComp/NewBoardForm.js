18e1d6859a3aa1c9a3ba4f0ce8ba147076a5996d
Alejandra <adom2128@gmail.com>
2023-07-18 08:50:37 -0600

    makes small changes to css

--- src/components/BoardListComp/NewBoardForm.js
+++ src/components/BoardListComp/NewBoardForm.js
@@ -31,11 +31,11 @@ const NewBoardForm = ({ onBoardFormSubmit }) => {
         <form className="new-board-form" onSubmit={handleSubmit}>
             <h2>Create New Board</h2>
             <div>        
-                <label className="new-board-form-label" htmlFor="newBoardTitle">Title:</label>
+                <label className="new-board-form-label" htmlFor="newBoardTitle">Title:  </label>
                 <input type="text" id="newBoardTitle" name="newBoardTitle" onChange={handleBoardTitleChange} value={newBoardTitle} />
             </div>
             <div> 
-                {/* <label className="new-board-form-label" htmlFor="newBoardOwner">Owner:</label> */}
+                <label className="new-board-form-label" htmlFor="newBoardOwner">Owner:</label>
                 <input type="text" id="newBoardOwner" name="newBoardOwner" onChange={handleBoardOwnerChange} value={newBoardOwner} />
             </div>
             {(newBoardTitle==='' || newBoardOwner==='') && <div className="warning"> Please fill in both fields. </div>}
